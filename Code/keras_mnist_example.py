ee0b3e89cb6a5683a72f919bb6bef4e4ad5fd2eb
Srinivas Tunuguntla <srinivas.tunuguntla92@gmail.com>
2017-04-01 14:30:43 -0500

    Added example keras code; Added code to pre-process cifar100

---
+++ Code/keras_mnist_example.py
@@ -0,0 +1,47 @@
+import numpy as np
+np.random.seed(123)
+
+import matplotlib.pyplot as plt
+
+import theano
+import keras
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+
+from keras.datasets import mnist
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+X_train /= 255.0
+X_test /= 255.0
+
+Y_train = np_utils.to_categorical(y_train, 10)
+Y_test = np_utils.to_categorical(y_test, 10)
+
+model = Sequential()
+
+model.add(Convolution2D(32, 3, 3, activation = 'relu', input_shape = (28, 28, 1)))
+model.add(Convolution2D(32, 3, 3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+
+model.add(Dense(128, activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation = 'softmax'))
+
+
+model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+model.fit(X_train, Y_train, batch_size = 32, nb_epoch = 10, verbose = 1)
+
+score = model.evaluate(X_test, Y_test, verbose = 1)
---
+++ Code/process_cifar100.py
@@ -0,0 +1,37 @@
+from keras.datasets import cifar100
+import matplotlib.pyplot as plt
+import cPickle
+from skimage import color
+import numpy as np
+import cPickle
+
+with open('../cifar100/meta', 'rb') as fmeta:
+    meta = cPickle.load(fmeta)
+    labels = meta['coarse_label_names']
+
+(X_train, y_train), (X_test, y_test) = cifar100.load_data('coarse')
+y_train = y_train.flatten()
+y_test = y_test.flatten()
+
+large_natural_outdoor_scenes_label = labels.index('large_natural_outdoor_scenes')
+
+X_train = X_train[y_train == large_natural_outdoor_scenes_label]
+X_test = X_test[y_test == large_natural_outdoor_scenes_label]
+
+del y_train, y_test
+
+X_train_lab = np.zeros(X_train.shape)
+X_test_lab = np.zeros(X_test.shape)
+
+for i in range(X_train.shape[0]):
+    X_train_lab[i] = color.rgb2lab(X_train[i])
+
+for i in range(X_test.shape[0]):
+    X_test_lab[i] = color.rgb2lab(X_test[i])
+
+X_train = X_train_lab
+X_test = X_test_lab
+
+data = (X_train, X_test)
+with open('cifar100_large_natural_outdoor_scenes.pickle', 'wb') as fp:
+    cPickle.dump(data, fp)
